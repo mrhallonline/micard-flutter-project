3cde27ffeda0cce7f78e24e53c688fba73efa2bd
Kevin <mrhallonline@gmail.com>
2019-10-30 00:22:19 -0500

    completed Mi Card project

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
@@ -23,21 +24,59 @@ class MyApp extends StatelessWidget {
                   fontFamily: 'Pacifico',
                   fontSize: 40.0,
                   color: Colors.white,
-                  fontWeight: FontWeight.bold,
+                  // fontWeight: FontWeight.bold,
                 ),
               ),
               Text(
                 'STEM Specialist',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    letterSpacing: 2.0,
-                    fontFamily:'Source_Sans_Pro',
-                    fontWeight: FontWeight.bold,
-                    color: Colors.teal[100],
+                style: TextStyle(
+                  fontSize: 20.0,
+                  letterSpacing: 2.0,
+                  fontFamily: 'Source_Sans_Pro',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal[100],
                 ),
-                
               ),
-            
+              SizedBox(
+                height: 20.0,
+                width: 100.0,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone_android,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '312-934-9688',
+                    style: TextStyle(
+                        color: Colors.teal[900],
+                        fontFamily: 'Source_Sans_Pro',
+                        fontSize: 20.0),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'hi@kevinhall.info',
+                    style: TextStyle(
+                        color: Colors.teal[900],
+                        fontFamily: 'Source_Sans_Pro',
+                        fontSize: 20.0),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
